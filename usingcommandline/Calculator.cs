73f15b33f91c9e3fcb03865f1754cfb2764c64f8
sureshnanjan <sureshnanjan@outlook.com>
2024-07-16 09:55:57 +0530

    Adding files for CSC understanding

---
+++ usingcommandline/Calculator.cs
@@ -0,0 +1,12 @@
+namespace utilities.math{
+public class Calculator{
+	public int add(){
+	System.Console.WriteLine("Doing addition");
+	return 0;
+	}
+	public int subtract(){
+	System.Console.WriteLine("Doing Subtraction");
+	return 0;
+	}
+}
+}
